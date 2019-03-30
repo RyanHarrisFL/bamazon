2f6f0d6791b8622a45e150314662b02e5fea8c65
Rhino <ryanharrisfl@gmail.com>
2019-03-29 20:47:08 -0400

    created seed sql

---
+++ seed.sql
@@ -0,0 +1,7 @@
+USE ice_creamDB;
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("vanilla", 2.50, 100);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("chocolate", 3.10, 120);
