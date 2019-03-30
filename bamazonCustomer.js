b60b16220261105bf974606f3f21f309347a0810
Rhino <ryanharrisfl@gmail.com>
2019-03-30 15:14:34 -0400

    cleaned up code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,71 +1,68 @@
 // Requiring npm packages
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-var Table = require('cli-table');
+var Table = require("cli-table");
 
 var table = new Table({
-    head: ['Product', 'Department', 'Price' , 'In Stock']
-  , colWidths: [40, 25, 25, 25]
+  head: ["ID", "Product", "Department", "Price", "In Stock"],
+  colWidths: [10, 60, 25, 10, 10]
 });
 
 //creating connection to Database
 const connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 8889,
-  
-    user: "root",
-  
-    password: "root",
-    database: "bamazon"
-  });
-  
-  // connect to the mysql server and sql database
-  connection.connect( (err) => {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+  host: "localhost",
 
-    displayMerch();
-    connection.end();
-  });
+  port: 8889,
+
+  user: "root",
+
+  password: "root",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(err => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+
+  displayMerch();
+  connection.end();
+});
 
 // Prompt asking the customer which items they would like to buy along with the quantity.
-  const prodRequest = () => {
-    inquirer.prompt([
-        {
-            name: "product",
-            type: "input",
-            message: "What is the ID number of the item you would like to purchase?",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                  return true;
-                }
-                return false;
-              }
+const prodRequest = () => {
+  inquirer.prompt([
+    {
+      name: "product",
+      type: "input",
+      message: "What is the ID number of the item you would like to purchase?",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
         }
-    ])
+        return false;
+      }
+    }
+  ]);
+};
 
-}
-
-  const displayMerch = () => {
-      connection.query("SELECT product_name, department_name, price, stock_quantity FROM products", (err, res) => {
-        if (err) throw err;
-        //console.log(res);
-        for (let i = 0; i < res.length; i++) {
-        table.push(
-            [res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-        );
-        }
-        console.log(table.toString());
-        prodRequest();
-      });
-      
-  }
-
-  
-
-  
-
-  
-  
-  
+const displayMerch = () => {
+  connection.query(
+    "SELECT id, product_name, department_name, price, stock_quantity FROM products",
+    (err, res) => {
+      if (err) throw err;
+      //console.log(res);
+      for (let i = 0; i < res.length; i++) {
+        table.push([
+          res[i].id,
+          res[i].product_name,
+          res[i].department_name,
+          res[i].price,
+          res[i].stock_quantity
+        ]);
+      }
+      console.log(table.toString());
+      prodRequest();
+    }
+  );
+};
