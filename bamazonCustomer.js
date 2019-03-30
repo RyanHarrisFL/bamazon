e3f45e2c47b65ef50cfcaabf5f29a39ba7329a2b
Rhino <ryanharrisfl@gmail.com>
2019-03-30 10:41:45 -0400

    confirmed connection with database. Also loaded inquirer and MySql packages.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,23 @@
+// Requiring npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+//creating connection to Database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 8889,
+  
+    user: "root",
+  
+    password: "root",
+    database: "bamazon"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    
+  });
+  
